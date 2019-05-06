2d47c34e81d25da928b407e51e4d3c108f5fe30c
Kevin Roberts-Godinho <39806291+KevinGodinho@users.noreply.github.com>
2019-05-05 20:58:16 -0700

    A lot of added features
    The tutorial walked me through adding quite a few features. The page now responds with updated ingredient amount data based on what the user adjusts the servings to. The list of recipes on the side highlights the current one that has been selected and the information for the recipe populates to the middle of the page.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
+import List from './models/List';
 import * as searchView from './views/searchView';
+import * as recipeView from './views/recipeView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 // Global State of App
@@ -9,9 +12,12 @@ import { elements, renderLoader, clearLoader } from './views/base';
 // Liked Recipes
 const state = {};
 
+
+// SEARCH CONTROLLER
+
 const controlSearch = async () => {
     // 1. Get query from view
-    const query = searchView.getInput(); 
+    const query = searchView.getInput();  
     
     if (query) {
         // 2. New search object and add to state
@@ -22,14 +28,21 @@ const controlSearch = async () => {
         searchView.clearResults();
         renderLoader(elements.searchRes);
         
-        // 4. Search for recipes
-        await state.search.getResults();
+        try {
+            // 4. Search for recipes
+            await state.search.getResults();
         
-        // 5. Render results on UI
-        clearLoader();
-        searchView.renderResults(state.search.result);
+            // 5. Render results on UI
+            clearLoader();
+            searchView.renderResults(state.search.result);
+            
+        } catch (err) {
+            alert('Something went wrong with the search...');
+            clearLoader();
+        }
     }
 }
+
 
 elements.searchForm.addEventListener('submit', e => {
     // this prevents the page from reloading
@@ -37,17 +50,78 @@ elements.searchForm.addEventListener('submit', e => {
     controlSearch();
 });
 
+elements.searchResPages.addEventListener('click', e => {
+    const btn = e.target.closest('.btn-inline');
+    if (btn) {
+        const goToPage = parseInt(btn.dataset.goto, 10);
+        searchView.clearResults();
+        searchView.renderResults(state.search.result, goToPage);
+        console.log(goToPage);
+    }
+});
 
 
+// RECIPE CONTROLLER
+
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
+    
+    if (id) {
+        // Prepare UI for changes
+        recipeView.clearRecipe();
+        renderLoader(elements.recipe);
+        
+        // Highlight selected search item
+        if (state.search) searchView.highlightSelected(id);
+        
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+        
+        try {
+            // Get recipe data and parse ingredients
+            await state.recipe.getRecipe();
+            state.recipe.parseIngredients();
+        
+             // Calculate servings and time
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+        
+            // Render recipe
+            clearLoader();
+            recipeView.renderRecipe(state.recipe);
+            
+        } catch (err) {
+            alert('Error processing recipe!');
+        }
+    }
+};
+
+// This is turned into one line of code below
+//window.addEventListener('hashchange', controlRecipe);
+//window.addEventListener('load', controlRecipe);
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
 
+// Handling recipe button clicks
+elements.recipe.addEventListener('click', e => {
+    if (e.target.matches('.btn-decrease, .btn-decrease *')) {
+        // Decrease button is clicked
+        if (state.recipe.servings > 1) {
+            state.recipe.updateServings('dec');
+            recipeView.updateServingsIngredients(state.recipe);
+        }
+    } else if (e.target.matches('.btn-increase, .btn-increase *')) {
+        // Increase button is clicked
+        state.recipe.updateServings('inc');
+        recipeView.updateServingsIngredients(state.recipe);
+    }
+});
 
 
-
-
-
-
-
+window.l = new List();
 
 
 
